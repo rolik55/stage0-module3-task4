4a4f60f1723553fe3817ec5deea4cc56ce9aea6d
rolik55 <65602933+rolik55@users.noreply.github.com>
2022-06-23 12:06:51 +0300

    v2

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        double a, b, result;
+        double a = 10, b = 20, result;
         result = (9 * (a * a) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
